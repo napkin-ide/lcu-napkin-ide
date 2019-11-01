f7b5dab48581601aa82836200ee3d128f9e26b0d
Robert Wise <robert.wise@fathym.com>
2019-11-01 14:37:37 -0600

    Added theming code to fix issue with Material styles not working.

--- projects/settings/src/app/app.component.ts
+++ projects/settings/src/app/app.component.ts
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public SelectedTheme: string;
+
+  constructor()  {}
+
+  public ngOnInit(): void {
+    this.resetTheme();
+  }
+
+  /**
+   * Reset material theme
+   */
+  protected resetTheme(): void {
+    this.changeTheme('ivy-light-theme');
+  }
+
+  /**
+   * Toggle through Fathym themes
+   *
+   * @param val theme type
+   */
+  protected changeTheme(val: string): void {
+    this.SelectedTheme = val;
+   }
 }
