f3de5a2e7aa4396b17e15a1ff18d6896769dc46e
mcgear <michael.gearhardt@fathym.com>
2019-11-06 07:18:05 -0700

    Small adjust

--- projects/setup/src/app/pages/org-review/org-review.component.ts
+++ projects/setup/src/app/pages/org-review/org-review.component.ts
@@ -8,15 +8,14 @@ import { NapkinIDESetupStateManagerContext } from '../../core/napkin-ide-setup-s
   styleUrls: ['./org-review.component.scss']
 })
 export class OrgReviewComponent implements OnInit {
+  // Properties
+  public get OAuthRedirectURL(): string {
+    return `${location.href}`;
+  }
 
-// Properties
-public get OAuthRedirectURL(): string {
-  return `${location.href}`;
-}
-
-public get AzureDevOpsOAuthURL(): string {
-  return `/.devops/oauth?redirectUri=${this.OAuthRedirectURL}`;
-}
+  public get AzureDevOpsOAuthURL(): string {
+    return `/.devops/oauth?redirectUri=${this.OAuthRedirectURL}`;
+  }
 
   /**
    * Setup step types
@@ -32,17 +31,10 @@ public get AzureDevOpsOAuthURL(): string {
   @Input('state')
   public State: NapkinIDESetupState;
 
-  constructor(protected nideState: NapkinIDESetupStateManagerContext) { }
+  constructor(protected nideState: NapkinIDESetupStateManagerContext) {}
 
   ngOnInit() {
-    /** Interval to check if infrastructure has been provisioned - prevents user from continuing until so. */
-    const finalizedInterval: any = setInterval(() => {
-      if (!this.State.CanFinalize && this.State.EnterpriseBooted) {
-        this.CanFinalize();
-      } else {
-        clearInterval(finalizedInterval);
-      }
-    }, 60000);
+    this.setupTempFinalizeTracker();
   }
 
   public Boot() {
@@ -61,4 +53,15 @@ public get AzureDevOpsOAuthURL(): string {
     this.nideState.CanFinalize();
   }
 
+  //  Helpers
+  protected setupTempFinalizeTracker() {
+    /** Interval to check if infrastructure has been provisioned - prevents user from continuing until so. */
+    const finalizedInterval: any = setInterval(() => {
+      if (!this.State.CanFinalize && this.State.EnterpriseBooted) {
+        this.CanFinalize();
+      } else {
+        clearInterval(finalizedInterval);
+      }
+    }, 30000);
+  }
 }
