0f1a9fb3482d1180d4ab05cc11e412f93e8ed97d
William Horst <william.horst@fathym.com>
2019-12-31 14:39:29 -0700

    user can add the fathym forecast lcu product - shows up in list of lcus by Architecture tab

--- projects/settings/src/app/pages/marketplace/product-view/product-view.component.ts
+++ projects/settings/src/app/pages/marketplace/product-view/product-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IdeSettingsState, LowCodeUnitSetupConfig } from '../../../core/ide-settings.state';
+import { IdeSettingsStateManagerContext } from '../../../core/ide-settings-state-manager.context';
 
 @Component({
   selector: 'lcu-product-view',
@@ -9,16 +11,36 @@ import { ActivatedRoute } from '@angular/router';
 export class MarketplaceProductViewComponent implements OnInit {
 
   public Product: any; // will be product model
+  public State: IdeSettingsState;
 
   constructor(
-    protected activatedRoute: ActivatedRoute
+    protected activatedRoute: ActivatedRoute,
+    protected ideSettingsState: IdeSettingsStateManagerContext
   ) {
     // Get the product info from the state instead of hard-coding it here:
-    this.Product = {Title: this.activatedRoute.snapshot.paramMap.get('product_title')};
+    this.Product = { Title: this.activatedRoute.snapshot.paramMap.get('product_title') };
     this.Product.Description = 'More description things yay! This is an API; spend the moneys and you can use it, because capitalism.';
   }
 
   ngOnInit() {
+    this.ideSettingsState.Context.subscribe(state => {
+      this.State = state;
+    });
+  }
+
+  public AddToOrg(product) {
+    this.State.Loading = true;
+    // map product into LowCodeUnitSetupConfig...
+    this.SaveLCU({
+      // hard coding this for now as functionality to populate this data from state is not yet complete
+      Lookup: 'lcu-fathym-forecast-lcu', // can be named anything
+      NPMPackage: '@habistack/lcu-fathym-forecast-lcu', // must be real package
+      PackageVersion: '0.9.75' // must be real version number
+    });
+  }
+
+  public SaveLCU(lcu: LowCodeUnitSetupConfig) {
+    this.ideSettingsState.SaveLCU(lcu);
   }
 
 }
