7088cacb0300c22a2f6cb259a2fe147968c705ee
powhound4 <george.hatch@fathym.com>
2020-03-24 15:58:35 -0600

    set up existing-user-accounts

---
+++ projects/platform-usage/src/app/pages/existing-user-accounts/existing-user-accounts.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Constants } from 'projects/platform-usage/src/lib/utils/constants';
+import { UserInfoModel } from 'projects/common/src/lcu.api';
+
+@Component({
+  selector: 'app-existing-user-accounts',
+  templateUrl: './existing-user-accounts.component.html',
+  styleUrls: ['./existing-user-accounts.component.scss']
+})
+export class ExistingUserAccountsComponent implements OnInit {
+
+  public UserData: Array<UserInfoModel>;
+
+  public DisplayedColumns: Array<string>;
+
+  constructor() { 
+    this.UserData = Constants.USER_DATA;
+    this.DisplayedColumns = ['Email', 'Username', 'FreeTrialSignUp', 'ExpirationDate'];
+  }
+
+  ngOnInit() {
+    console.log("UserData",Constants.USER_DATA);
+    
+  }
+
+}
