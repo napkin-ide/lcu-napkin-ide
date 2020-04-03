e925f400980d78710875053b53cdbf1a790a5e69
powhound4 <george.hatch@fathym.com>
2020-04-03 16:30:30 -0600

    resolve errors

--- projects/billing/src/app/pages/pages.module.ts
+++ projects/billing/src/app/pages/pages.module.ts
@@ -2,14 +2,14 @@ import { MaterialModule, PipeModule } from '@lcu/common';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { UserComponent } from './user/user.component';
+import { BillingComponent } from './billing/billing.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CompleteComponent } from './complete/complete.component';
 import { LcuNapkinIdeModule } from '@napkin-ide/lcu-napkin-ide-common';
 
 @NgModule({
-  declarations: [UserComponent, CompleteComponent],
+  declarations: [BillingComponent, CompleteComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -20,7 +20,7 @@ import { LcuNapkinIdeModule } from '@napkin-ide/lcu-napkin-ide-common';
     LcuNapkinIdeModule,
     PipeModule
   ],
-  exports: [UserComponent],
+  exports: [BillingComponent],
   providers: []
 })
 export class PagesModule {}
