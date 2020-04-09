7bc1f12f0031f8456552d5bd4341edc9de63c924
powhound4 <george.hatch@fathym.com>
2020-04-09 16:53:07 -0600

    set up billing flow components

---
+++ projects/billing/src/app/pages/billing-flow/billing-flow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lcu-billing-flow',
+  templateUrl: './billing-flow.component.html',
+  styleUrls: ['./billing-flow.component.scss']
+})
+export class BillingFlowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
