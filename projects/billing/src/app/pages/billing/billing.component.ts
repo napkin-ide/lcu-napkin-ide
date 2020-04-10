4c7997be56f830f3ea4e0f15f6d7e12739b63f34
mcgear <michael.gearhardt@gmail.com>
2020-04-10 15:11:02 -0600

    Change location of stripe public key until settings are implemented

--- projects/billing/src/app/pages/billing/billing.component.ts
+++ projects/billing/src/app/pages/billing/billing.component.ts
@@ -60,6 +60,14 @@ export class BillingComponent
  */
   protected planID: any;
 
+  protected get stripePublicKey(): string {
+    const stateCfg: any = this.lcuSettings.StateConfig;
+
+
+
+    return stateCfg && stateCfg.Stripe ? stateCfg.Stripe.PublicKey : '';
+  }
+
   //  Properties
   public BillingForm: FormGroup;
 
@@ -88,11 +96,11 @@ export class BillingComponent
     protected route: ActivatedRoute,
   ) {
     this.State = {};
-   
+
     this.route.paramMap.subscribe(params => {
-      this.planID = params.get('id');  
+      this.planID = params.get('id');
     });
-    
+
   }
 
   //  Life Cycle
@@ -180,10 +188,10 @@ public ResetBillingStatus(){
   }
 
   protected setupStripe() {
-    
+
     if (!this.stripe) {
       // Your Stripe public key
-      this.stripe = Stripe(this.lcuSettings.Settings.Stripe.PublicKey);
+      this.stripe = Stripe(this.stripePublicKey);
       // this.setupStripeElements();
       const elements = this.stripe.elements();
 
@@ -240,7 +248,7 @@ public ResetBillingStatus(){
     }
   }
 
-  
+
 
   protected setupStripeElements():void{
     const elements = this.stripe.elements();
@@ -312,7 +320,7 @@ public ResetBillingStatus(){
     // using *ngIf with external form properties
     this.cdr.detectChanges();
 
-    
+
 
     if (this.State.PaymentStatus) {
       console.log("Payment Status",this.State.PaymentStatus)
