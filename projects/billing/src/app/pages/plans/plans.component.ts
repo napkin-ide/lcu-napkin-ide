dbef8cefdc36ac2d30e86c153298199f460d0c02
mcgear <michael.gearhardt@gmail.com>
2020-04-14 16:23:04 -0600

    Checkpoint

--- projects/billing/src/app/pages/plans/plans.component.ts
+++ projects/billing/src/app/pages/plans/plans.component.ts
@@ -23,37 +23,39 @@ export class PlansComponent implements OnInit {
       this.ShowButton = true;
      }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.userBillState.Context.subscribe((state: any) => {
       this.State = state;
+
+      console.log('Plans: ', this.State.Plans);
+
       this.stateChanged();
     });
-    console.log('Plans: ',this.State.Plans);
-  
+
   }
 
-  public BuyNowClicked(plan: any){
-    console.log("Buy Now Clicked:", plan);
+  public BuyNowClicked(plan: any) {
+    console.log('Buy Now Clicked:', plan);
     this.router.navigate(['plan', plan.Lookup]);
-    
+
   }
 
-  protected stateChanged(): void{
-    console.log("state plan page = ", this.State);
-    //TODO below code causes infinite loop
+  protected stateChanged(): void {
+    console.log('state plan page = ', this.State);
+    // TODO below code causes infinite loop
     // if(!this.State.Loading && this.State.PaymentStatus.Code === 0){
     //   this.userBillState.ResetState();
     //   console.log("State after resetting state: ", this.State);
     // }
 
-    if(this.State.Plans){
+    if (this.State.Plans) {
       this.PlanGroups = new Array<BillingPlanOption>();
       this.State.Plans.forEach((plan: BillingPlanOption) => {
-        if(plan.Interval === 'month'){
+        if (plan.Interval === 'month') {
           this.PlanGroups.push(plan);
         }
       });
-      console.log("plan groups", this.PlanGroups);
+      console.log('plan groups', this.PlanGroups);
     }
   }
 
