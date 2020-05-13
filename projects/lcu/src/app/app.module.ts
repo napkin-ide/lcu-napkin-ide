d86de1594647156ebc28f636fe4296adfe86ebaa
mcgear <michael.gearhardt@gmail.com>
2020-05-13 17:50:13 -0600

    Fix for missing LCUServiceSettings

--- projects/lcu/src/app/app.module.ts
+++ projects/lcu/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule, DoBootstrap, Injector } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { createCustomElement } from '@angular/elements';
-import { FathymSharedModule } from '@lcu/common';
+import { FathymSharedModule, LCUServiceSettings } from '@lcu/common';
 import { BrowserModule } from '@angular/platform-browser';
 import {
   LcuNapkinIdeModule,
   SelectorLcuNapkinIdeShellHostElement,
-  LcuNapkinIdeShellHostElementComponent, LcuNapkinIdeWelcomeJourneysElementComponent, SELECTOR_LCU_NAPKIN_IDE_WELCOME_JOURNEYS_ELEMENT
+  LcuNapkinIdeShellHostElementComponent,
+  LcuNapkinIdeWelcomeJourneysElementComponent,
+  SELECTOR_LCU_NAPKIN_IDE_WELCOME_JOURNEYS_ELEMENT,
 } from '@napkin-ide/lcu-napkin-ide-common';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, BrowserAnimationsModule, FathymSharedModule, LcuNapkinIdeModule],
-  exports: [LcuNapkinIdeModule]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FathymSharedModule,
+    LcuNapkinIdeModule,
+  ],
+  exports: [LcuNapkinIdeModule],
+  providers: [
+    {
+      provide: LCUServiceSettings,
+      useValue: FathymSharedModule.DefaultServiceSettings(environment),
+    },
+  ],
 })
 export class AppModule implements DoBootstrap {
   //  Constructors
@@ -20,12 +34,20 @@ export class AppModule implements DoBootstrap {
 
   //  Life Cycle
   public ngDoBootstrap() {
-    const dfMgr = createCustomElement(LcuNapkinIdeShellHostElementComponent, { injector: this.injector });
+    const dfMgr = createCustomElement(LcuNapkinIdeShellHostElementComponent, {
+      injector: this.injector,
+    });
 
     customElements.define(SelectorLcuNapkinIdeShellHostElement, dfMgr);
-  
-		const welcomeJourneys = createCustomElement(LcuNapkinIdeWelcomeJourneysElementComponent, { injector: this.injector });
 
-		customElements.define(SELECTOR_LCU_NAPKIN_IDE_WELCOME_JOURNEYS_ELEMENT, welcomeJourneys);
-	}
+    const welcomeJourneys = createCustomElement(
+      LcuNapkinIdeWelcomeJourneysElementComponent,
+      { injector: this.injector }
+    );
+
+    customElements.define(
+      SELECTOR_LCU_NAPKIN_IDE_WELCOME_JOURNEYS_ELEMENT,
+      welcomeJourneys
+    );
+  }
 }
