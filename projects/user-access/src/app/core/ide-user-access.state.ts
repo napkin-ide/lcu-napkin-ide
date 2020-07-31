466454657cd45f7745fcae7ea0645962f052cc28
xchanin <me@shannonbruns.com>
2020-07-31 09:55:43 -0600

    add progress bar and some style

--- projects/user-access/src/app/core/ide-user-access.state.ts
+++ projects/user-access/src/app/core/ide-user-access.state.ts
@@ -3,8 +3,12 @@ export class IdeUserAccessState {
 
   public Loading?: boolean;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
   public RequestAuthorizationSent?: string;
+=======
+  public RequestAuthorizationSent?: any;
+>>>>>>> Stashed changes
 =======
   public RequestAuthorizationSent?: any;
 >>>>>>> Stashed changes
