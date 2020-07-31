3723de7b5e54fb0b4cb58cd22c4483d13e033ff8
xchanin <me@shannonbruns.com>
2020-07-31 10:23:42 -0600

    fix

--- projects/user-access/src/app/core/ide-user-access.state.ts
+++ projects/user-access/src/app/core/ide-user-access.state.ts
@@ -2,14 +2,6 @@
 export class IdeUserAccessState {
 
   public Loading?: boolean;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
   public RequestAuthorizationSent?: string;
-=======
-  public RequestAuthorizationSent?: any;
->>>>>>> Stashed changes
-=======
-  public RequestAuthorizationSent?: any;
->>>>>>> Stashed changes
 }
